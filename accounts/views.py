6994cc8c3439dd63efa327758e0899cde1907e0f
AbdulRahman <abdurahmanabdul9@gmail.com>
2025-09-04 10:00:56 +0700

    What I push is Now you have the full authentication flow UIs design:
    1. Login
    2. Register
    3. Logout confirmation
    4. Forgot Password
    5. Reset Password
    and adjust settings and so on

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def login(request):
+    return render(request, 'accounts/login.html', {})
+
+def register(request):
+    return render(request, 'accounts/register.html', {})
+
+def logout(request):
+    return render(request, 'accounts/logout.html', {})
+
+def forgot_password(request):
+    return render(request, 'accounts/forgot_password.html', {})
+
+def reset_password(request):
+    return render(request, 'accounts/reset_password.html', {})

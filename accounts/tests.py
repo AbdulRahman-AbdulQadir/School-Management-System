492579d0ba684755a2817cf8147bde1702ef16e9
Ziad Alslahe <alslaheziad@gmail.com>
2025-09-08 17:53:07 +0300

    Translate all messages from Arabic to English and update auth templates

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your tests here.
+class AuthTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="testuser", password="testpass")
+
+    def test_login_success(self):
+        response = self.client.post(reverse("login"), {"username": "testuser", "password": "testpass"})
+        self.assertRedirects(response, "/home/")
+
+    def test_login_failure(self):
+        response = self.client.post(reverse("login"), {"username": "testuser", "password": "wrongpass"})
+        self.assertRedirects(response, "/login/")
+
+    def test_logout(self):
+        self.client.login(username="testuser", password="testpass")
+        response = self.client.get(reverse("logout"))
+        self.assertRedirects(response, "/login/")

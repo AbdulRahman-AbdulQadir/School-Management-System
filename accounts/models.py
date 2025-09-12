b41e51a0dc4aafbaf2a21bb8fbf3c79ddb32372b
Mohammed Younis <156939965+DevMo7md@users.noreply.github.com>
2025-09-12 23:07:27 +0200

    add Profile & Update Profile pages & Make AbstractUser(Profile photo & Phone num & National ID,Photo)

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+import uuid
 # Create your models here.
+class CustomUser(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    national_id = models.CharField(max_length=20, unique=True)
+    national_id_image = models.ImageField(upload_to='national_id_images/', null=True, blank=True)
+    profile_image = models.ImageField(upload_to='profile_images/', null=True, blank=True)
+    phone_number = models.CharField(max_length=15, unique=True)
+    is_student = models.BooleanField(default=False)
+    is_teacher = models.BooleanField(default=False)
+    is_parent = models.BooleanField(default=False)
+
+    
+
+    def __str__(self):
+        return self.username
+    

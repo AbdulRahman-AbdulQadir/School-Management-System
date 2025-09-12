f5541aa8680e2a65f3ce30f7e5d3a1283dd3482d
Mohammed Younis <156939965+DevMo7md@users.noreply.github.com>
2025-09-12 23:07:27 +0200

    add Profile & Update Profile pages & Make AbstractUser(Profile photo & Phone num & National ID,Photo)

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('forgot_password/', views.forgot_password, name='forgot_password'),
     path('verify_code/', views.verify_code, name='verify_code'),
     path('reset_password/', views.reset_password, name='reset_password'),
+    path('profile/', views.profile, name='profile'),
+    path('profile/update/', views.profile_update, name='profile_update'),
 ]

47ba102b3ff01074f1901ebd8edc27785a1119e0
AbdulRahman <abdurahmanabdul9@gmail.com>
2025-11-16 08:53:02 +0700

    I did my task which is Updating the dashboard to show the real number of pending users.

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from accounts.models import CustomUser
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import PermissionDenied
 from django.contrib import messages
@@ -13,4 +14,9 @@ def admin_dashboard(request):
     if not (request.user.is_staff or request.user.is_superuser):
         messages.error(request, 'You do not have permission to access the admin dashboard.')
         return render(request, 'pages/home.html', {})
-    return render(request, 'pages/admin_dashboard.html', {})
+    # Count users who registered but are not yet approved by admin
+    pending_count = CustomUser.objects.filter(is_member_of_this_school=False).count()
+    context = {
+    "pending_count": pending_count,
+    }
+    return render(request, 'pages/admin_dashboard.html', context)
